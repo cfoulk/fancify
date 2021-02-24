c2106c910d9dce2b658f032d8cff7ec9068d3ba2
cfoulk <charlesfoulk@csus.edu>
2021-02-23 22:26:58 -0800

    trying something

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('button');
 
 btn.onclick = function() {
-    alert("Hello, world!");
+    //alert("Hello, world!");
+    document.getElementById("text").style.fontSize = "24px";
 }
