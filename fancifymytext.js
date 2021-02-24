c5d7b934292b360867214aacac758b71ffe8c88f
cfoulk <charlesfoulk@csus.edu>
2021-02-23 23:48:45 -0800

    testing

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector('button');
 const btn2 = document.getElementById("button 2");
 
+const slt = document.getElementById("slt");
+
+
 btn.onclick = function() {
     //alert("Hello, world!");
     document.getElementById("mybox").style.fontSize = "24px";
@@ -8,4 +11,11 @@ btn.onclick = function() {
 
 btn2.onclick = function() {
     alert("Radio button");
+}
+
+
+function changeBox() {
+    document.getElementById("mybox").style.fontWeight = "bold";
+    document.getElementById("mybox").style.textDecoration = "underline";
+    document.getElementById("mybox").style.color = "blue";
 }
